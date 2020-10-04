da570f1e315f95b9581b75871d57326dd39ebd09
jungai <juworaphol125@gmail.com>
2020-10-04 19:49:33 +0700

    init config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    en: {
+        "browser": true,
+        "es6": true
+    },
+    parser: "babel-eslint",
+    extends: [
+        "airbnb",
+        "prettier",
+        "prettier/react"
+    ],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    plugins: [
+        "react",
+        "prettier"
+    ],
+    rules: {
+        "prettier/prettier": ["error", {
+            trailingComma: "all",
+            semi: true,
+            arrowParens: "always",
+            singleQuote: true
+        }],
+        "react/react-in-jsx-scope": "off"
+    }
+}
